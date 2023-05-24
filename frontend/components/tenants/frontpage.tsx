334490303c85ace0be5aff95e3e25fb7d0c00fe3
frustrabe <lukas.forner@gmail.com>
2023-05-24 11:48:31 +0200

    final styling of tenant front page

--- frontend/components/tenants/frontpage.tsx
+++ frontend/components/tenants/frontpage.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { AppDispatch, RootState } from "../../store";
 import { useSelector, useDispatch } from "react-redux";
-import { View, Text, TextInput, StyleSheet, Button, Image } from "react-native";
+import { View, TextInput, StyleSheet, Button, Image } from "react-native";
 import { findOne, login, signup, updateToken } from "../users/usersSlice";
 import { UsersEntity } from "../users/usersEntity";
 import * as SecureStore from "expo-secure-store";
 import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
 import { IconComponentProvider, Icon } from "@react-native-material/core";
+import { Text, Divider, useTheme } from "@rneui/themed";
 
 export function FrontpageTenant() {
   const token: string | null | undefined = useSelector(
@@ -73,8 +74,9 @@ export function FrontpageTenant() {
   };
 
   return (
-    <ScrollView>
-      <View style={styles.searchBar}>
+    <View style={{ backgroundColor: "white" }}>
+      <ScrollView>
+        {/* <View style={styles.searchBar}>
         <TextInput
           style={styles.searchInput}
           placeholder="Søg efter dokumenter, artikler m.m"
@@ -82,126 +84,84 @@ export function FrontpageTenant() {
         ></TextInput>
         <Image source={require("../../assets/search.png")}></Image>
       </View>
-
-      <View style={styles.mainContainer}>
-        <View style={styles.container}>
-          <View>
-            <Image
-              style={styles.image}
-              source={require("../../assets/customer-service.png")}
-            ></Image>
-          </View>
-          <View style={styles.bubbleText}>
-            <Text>{text1.title}</Text>
-            <Text style={styles.text}>{text1.time}</Text>
-          </View>
+ */}
+        <View style={{ marginLeft: 20 }}>
+          <Text style={styles.title} h4>
+            Resident support
+          </Text>
         </View>
+        <View style={styles.mainContainer}>
+          <View style={styles.container}>
+            <View>
+              <Image
+                style={styles.image}
+                source={require("../../assets/customer-service.png")}
+              ></Image>
+            </View>
+            <View style={styles.bubbleText}>
+              <Text>{text1.title}</Text>
+              <Text style={styles.text}>{text1.time}</Text>
+            </View>
+          </View>
 
-        <View style={styles.container}>
-          <View style={styles.image}>
-            <Image source={require("../../assets/office-service.png")}></Image>
+          <View style={styles.container}>
+            <View style={styles.image}>
+              <Image
+                source={require("../../assets/office-service.png")}
+              ></Image>
+            </View>
+            <View style={styles.bubbleText}>
+              <Text>{text2.title}</Text>
+              <Text style={styles.text}>{text2.time}</Text>
+            </View>
           </View>
-          <View style={styles.bubbleText}>
-            <Text>{text2.title}</Text>
-            <Text style={styles.text}>{text2.time}</Text>
-          </View>
-        </View>
 
-        <View style={styles.problems}>
-          <View>
-            <Text style={styles.title}>My Problems</Text>
+          <View style={styles.problems}>
+            <View>
+              <Text style={styles.title} h4>
+                My Problems
+              </Text>
+            </View>
+            <View>
+              <Text style={styles.seeAll}>See all →</Text>
+            </View>
           </View>
-          <View>
-            <Text style={styles.seeAll}>See all →</Text>
-          </View>
-        </View>
 
-        <ScrollView horizontal={true} style={{ marginBottom: 20 }}>
-          <View
-            style={{
-              marginLeft: 15,
-              marginRight: 15,
-              marginTop: 15,
-            }}
-          >
-            <View
-              style={{
-                alignSelf: "center",
-              }}
-            >
+          <ScrollView horizontal={true}>
+            <View style={styles.myProblemsContainer}>
               {user &&
                 user.tenant.problem.map((problem: any) => (
-                  <View
-                    style={{
-                      width: "100%",
-                      margin: 20,
-                      borderRadius: 17,
-                      borderColor: "#F2F4F7",
-                      borderStyle: "dashed",
-                      backgroundColor: "#F2F4F7",
-                      borderWidth: 2,
-                      padding: 20,
-                    }}
-                  >
-                    <View
-                      style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        alignItems: "center",
-                        width: "128%",
-                      }}
-                    >
-                      <View
-                        style={{
-                          display: "flex",
-                          flexDirection: "row",
-                          width: "70%",
-                          alignItems: "center",
-                          marginTop: 5,
-                        }}
-                      >
-                        <Text
-                          h3
-                          style={{
-                            marginBottom: 7,
-                          }}
-                        >
+                  <View>
+                    <View style={styles.problemContainer}>
+                      <View>
+                        <Text style={styles.problemContainerTitle} h4>
                           {problem.subject}
                         </Text>
-                        <Text
-                          style={{
-                            width: "100%",
-                          }}
-                        >
-                          - DEPARTMENT
+                        <Text style={styles.problemContainerDepartment}>
+                          {problem.department}
                         </Text>
                       </View>
+                      <Divider
+                        style={{ marginRight: 10, marginLeft: 10 }}
+                        width={1}
+                        color="#D0D5DD"
+                      />
+                      <Image
+                        style={styles.problemContainerImage}
+                        source={{ uri: `${problem.image}` }}
+                      />
                     </View>
-                    <Image
-                      style={{
-                        width: 100,
-                        height: 200,
-                        alignSelf: "center",
-                      }}
-                      source={{ uri: `${problem.image}` }}
-                    />
                   </View>
                 ))}
             </View>
-            {/*  {user.tenant.problem.map((problem: any) => (
-                <>
-                  <Text>{problem.subject}</Text>
-                  <Text>{problem.description}</Text>
-                </>
-              ))} */}
-          </View>
-        </ScrollView>
-      </View>
-    </ScrollView>
+          </ScrollView>
+        </View>
+      </ScrollView>
+    </View>
   );
 }
 const styles = StyleSheet.create({
-  searchInput: {
+  /* searchInput: {
     height: 50,
     margin: 12,
     borderWidth: 1,
@@ -213,7 +173,7 @@ const styles = StyleSheet.create({
   searchBar: {
     flex: 1,
     flexDirection: "row",
-  },
+  }, */
   text: {
     fontSize: 20,
     marginTop: 10,
@@ -224,14 +184,12 @@ const styles = StyleSheet.create({
     fontSize: 20,
   },
   seeAll: {
-    marginTop: 20,
-    fontSize: 10,
+    marginTop: 25,
+    fontSize: 15,
     fontWeight: "bold",
   },
   container: {
     flex: 1,
-    margin: 3,
-    marginTop: 10,
     marginBottom: 10,
     padding: 20,
     borderRadius: 15,
@@ -240,8 +198,9 @@ const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
     flexDirection: "column",
-    margin: 10,
-    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    padding: 10,
     justifyContent: "space-evenly",
     borderRadius: 15,
   },
@@ -265,4 +224,41 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 5,
   },
+  problemContainer: {
+    width: 250,
+    marginRight: 10,
+    marginTop: 10,
+    borderRadius: 15,
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderColor: "#D0D5DD",
+
+    padding: 10,
+  },
+  problemContainerTitle: {
+    display: "flex",
+    padding: 10,
+    justifyContent: "flex-start",
+  },
+  problemContainerDepartment: {
+    display: "flex",
+    paddingLeft: 10,
+    marginBottom: 10,
+    justifyContent: "flex-start",
+  },
+  problemContainerImage: {
+    marginTop: 15,
+    marginBottom: 10,
+    padding: 10,
+    width: 210,
+    height: 250,
+    alignSelf: "center",
+    borderRadius: 15,
+  },
+  myProblemsContainer: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+  },
+  divider: {},
 });
